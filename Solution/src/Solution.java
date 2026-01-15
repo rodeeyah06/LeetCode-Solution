44daa3abb4fa72c88114065bb115ebb07c6da4b7
rodeeyah06 <musarodiyah04@gmail.com>
2026-01-15 02:54:02 -0800

    Add files via upload

---
+++ Solution/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Vector;
+
+public class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length-1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+
+        return nums;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
